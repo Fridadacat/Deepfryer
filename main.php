a20ea64281d818db9cc44b1ac71d3983fd8eedd6
Fridadacat <49615077+Fridadacat@users.noreply.github.com>
2023-10-18 15:25:24 +0200

    Created Hello World

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+$helloWorld = "<!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Hello World</title>
+                    </head>
+                    <body>
+                        <h1>Hello World</h1>
+                    </body>
+                </html>";
+
+echo $helloWorld;
+?>
