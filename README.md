4df5731c11480633cd0086ba98052799728c95c8
Fridadacat <49615077+Fridadacat@users.noreply.github.com>
2023-10-22 17:42:19 +0200

    Added the fry function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Deepfryer
+layout:
+
+Entpacke alle dateien in deinen htdocs folder und passe den dateipfad in fry.php an
  
+add GD https://www.geeksforgeeks.org/how-to-install-php-gd-in-xampp/
+https://getcomposer.org/download/ => download composer setup
+cmd => composer require mirazmac/php-deep-fry
--- fry.php
+++ fry.php
@@ -26,14 +26,23 @@
         <div class="row">
             <div class="col-md-12 center-image">
                 <?php
+                require 'C:/xampp/htdocs/vendor/autoload.php';
+
                 if (isset($_FILES['uploaded_image'])) {
-                    $upload_dir = 'C:/xampp/htdocs/'; // Passe diesen Pfad an, um zum htdocs-Ordner deines Servers zu gelangen.
+                    $upload_dir = 'C:/xampp/htdocs/'; 
 
                     $file_name = $_FILES['uploaded_image']['name'];
                     $file_path = $upload_dir . $file_name;
 
                     if (move_uploaded_file($_FILES['uploaded_image']['tmp_name'], $file_path)) {
-                        echo "<img src='$file_name' alt='Frittiertes Bild' class='responsive-image'>";
+                        $fryer = new MirazMac\DeepFry\Fryer($file_path);
+                        $picture = $fryer->fry()
+                            ->moreDeepNibba()
+                            ->quality(20)
+                            ->save();
+
+                        $improved_file_path = 'http://localhost/'.pathinfo($file_name, PATHINFO_FILENAME).'_deepfried.jpg';
+                        echo "<img src='$improved_file_path' alt='Frittiertes Bild' class='responsive-image'>";
                     } else {
                         echo "Fehler beim Hochladen des Bildes.";
                     }
